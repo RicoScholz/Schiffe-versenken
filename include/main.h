49dcb855387afe1de49ed2c4d504a8b4a5efd6c6
Rico Scholz <rico.scholz@arcor.de>
2023-02-03 18:29:18 +0100

    feat(generateShips): ship length and oriantation

--- include/main.h
+++ include/main.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
 
 void initGame();
+void generateShips(int board[10][10]);
 void showBoard(int p1[10][10], int com[10][10]);
--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdbool.h>
 #include <stdlib.h>
 #include <time.h>
 
@@ -7,6 +7,7 @@
 
 int main()
 {
+    srand(time(NULL));
     initGame();
 
     return 0;
@@ -17,63 +18,104 @@ int main()
 // 1 = Schiff
 // 2 = Treffer
 // 3 = Wasser (Kein Treffer)
-void initGame() {
+void initGame()
+{
     int player[10][10];
     int computer[10][10];
 
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            player[i][j] = 0;
-            computer[i][j] = 0;
+    for (int row = 0; row < 10; row++)
+    {
+        for (int col = 0; col < 10; col++)
+        {
+            player[col][row] = 0;
+            computer[col][row] = 0;
         }
     }
 
     generateShips(player);
+    generateShips(computer);
 
     showBoard(player, computer);
 }
 
+
+// Generiert ein zufälliges Schiff und Plaziert es in einem Array
 void generateShips(int board[10][10])
 {   
     int x, y;
-    int ships = 10;
+    bool horizontal = true;
+    bool validShip = true;
+    int ships[5] = {2, 3, 3, 4, 5};
 
-    srand(time(NULL));
-
-    for (int i = 0; i < ships; i++)
+    for (int c = 0; c < 5; c++)
     {
-        x = rand() % 10;
-        y = rand() % 10;
+        horizontal = rand() & 1;
 
-        if (board[x][y] != 0)
-        {   
-            i--;
+        if (horizontal) {
+            x = rand() % (10 - ships[c]);
+            y = rand() % (10);
+        } else {
+            x = rand() % (10);
+            y = rand() % (10 - ships[c]);
+        }
+
+        for (int j = -1; j <= 1; j++)
+        {
+            for (int i = -1; i < ships[c] + 1; i++)
+            {
+                if (horizontal) {
+                    if (board[x + i][y + j] != 0) 
+                    {   
+                        validShip = false;
+                    }
+                } else {
+                    if (board[x + j][y + i] != 0) 
+                    {   
+                        validShip = false;
+                    }
+                }
+            }
+        }
+
+        if (!validShip) {
+            validShip = true;
+            c--;
             continue;
         }
-        
-        board[x][y] = 1;   
+
+        for (int i = 0; i < ships[c]; i++)
+        {
+            if (horizontal) {
+                board[x + i][y] = 1;
+            } else {
+                board[x][y + i] = 1;
+            }
+        }
     }
 }
 
-
 // Zeigt zwei Felder passend Formatiert in der Konsole an
-// 
-//
-//
+// " " = Leer
+// "S" = Schiff
+// "X" = Treffer
+// "O" = Wasser (Kein Treffer)
 void showBoard(int p1[10][10], int com[10][10])
 {
     // Clear Screen
-    printf("\e[1;1H\e[2J");
+    // printf("\e[1;1H\e[2J");
 
     // Oben
-    printf("+----------+          +----------+\n");
+    printf("   ABCDEFGHIJ              ABCDEFGHIJ \n");
+    printf("  +----------+            +----------+\n");
 
     //Felder
-    for (int i = 0; i < 10; i++) {
-        printf("|");
-        for (int j = 0; j < 10; j++) {
+    for (int row = 0; row < 10; row++)
+    {
+        printf("%2d|", row+1);
+        for (int col = 0; col < 10; col++)
+        {
             char tile = ' ';
-            switch(p1[i][j]) {
+            switch(p1[col][row]) {
                 case 0: tile = ' '; break;
                 case 1: tile = 'S'; break;
                 case 2: tile = 'X'; break;
@@ -82,13 +124,13 @@ void showBoard(int p1[10][10], int com[10][10])
             printf("%c", tile);
         }
 
-        printf("|          |");
+        printf("|          %2d|", row+1);
 
-        for (int j = 0; j < 10; j++) {
+        for (int col = 0; col < 10; col++)
+        {
             char tile = ' ';
-            switch(com[i][j]) {
+            switch(com[col][row]) {
                 case 0: tile = ' '; break;
-                case 1: tile = 'S'; break;
                 case 2: tile = 'X'; break;
                 case 3: tile = 'O'; break;
             }
@@ -98,5 +140,5 @@ void showBoard(int p1[10][10], int com[10][10])
     }
 
     // Unten
-    printf("+----------+          +----------+\n");
+    printf("  +----------+            +----------+\n");
 }
