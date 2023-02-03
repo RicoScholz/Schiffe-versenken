c4fbe01621ff1a8284df2228c234d9f6573f7b98
Rico Scholz <rico.scholz@arcor.de>
2023-02-03 20:40:55 +0100

    feat(turns): player and computer can fire on their enemy

--- include/main.h
+++ include/main.h
@@ -1,5 +1,7 @@
-#include <stdio.h>
+#include <stdbool.h>
 
-void initGame();
+void initGame(bool *gameOver, bool *playerTurn, int b1[10][10], int b2[10][10]);
+bool playerTurn(int enemy[10][10]);
+bool computerTurn(int enemy[10][10]);
 void generateShips(int board[10][10]);
-void showBoard(int p1[10][10], int com[10][10]);
+void showBoards(int b1[10][10], int b2[10][10]);
--- src/main.c
+++ src/main.c
@@ -7,8 +7,34 @@
 
 int main()
 {
+    // Initialisiert den RNG mit einem Seed
     srand(time(NULL));
-    initGame();
+
+    // Deklatieren und Initialisieren der Variablen
+    bool pTurn, gameOver;
+    int player[10][10], computer[10][10];
+    initGame(&gameOver, &pTurn, player, computer);
+
+    // Gameloop
+    showBoards(player, computer);
+    while (!gameOver) {
+
+        if (pTurn)
+        {
+            if (!playerTurn(computer))
+            {
+                pTurn = !pTurn;
+            }
+        }
+        else 
+        {
+            if (!computerTurn(player))
+            {
+                pTurn = !pTurn;
+            }
+        }
+        showBoards(player, computer);
+    }
 
     return 0;
 }
@@ -18,26 +44,98 @@ int main()
 // 1 = Schiff
 // 2 = Treffer
 // 3 = Wasser (Kein Treffer)
-void initGame()
+void initGame(bool *gameOver, bool *pTurn, int b1[10][10], int b2[10][10])
 {
-    int player[10][10];
-    int computer[10][10];
+    *gameOver = false;
+    *pTurn = rand() & 1;
 
     for (int row = 0; row < 10; row++)
     {
         for (int col = 0; col < 10; col++)
         {
-            player[col][row] = 0;
-            computer[col][row] = 0;
+            b1[col][row] = 0;
+            b2[col][row] = 0;
         }
     }
 
-    generateShips(player);
-    generateShips(computer);
-
-    showBoard(player, computer);
+    generateShips(b1);
+    generateShips(b2);
 }
 
+// Fordert den Spieler auf Koordinaten einzugeben
+// Mapt Koordinaten auf das Array
+bool playerTurn(int enemy[10][10])
+{
+    char coords[4];
+    printf("Koodinaten eigeben: ");
+    scanf("%3s", coords);
+
+    int x;
+    switch (coords[0])
+    {
+        case 'A': x = 0; break;
+        case 'B': x = 1; break;
+        case 'C': x = 2; break;
+        case 'D': x = 3; break;
+        case 'E': x = 4; break;
+        case 'F': x = 5; break;
+        case 'G': x = 6; break;
+        case 'H': x = 7; break;
+        case 'I': x = 8; break;
+        case 'J': x = 9; break;
+        default: break;
+    }
+
+    int y = coords[1] - '1';
+    if (coords[1] = '1' && coords[2] == '0')
+    {
+        y = 9;
+    }
+
+    switch (enemy[x][y])
+    {
+        // Verfehlt
+        case 0: 
+            enemy[x][y] = 3;
+            return false;
+
+        // Treffer -> erneuter Schuss
+        case 1: 
+            enemy[x][y] = 2; 
+            return true;
+
+        // bereits getroffenes Feld -> erneuter Versuch
+        default: 
+            printf("Ungültige Eingabe!");
+            playerTurn(enemy);
+            break;
+    }
+}
+
+// Schießt auf ein Zufälliges Feld
+bool computerTurn(int enemy[10][10])
+{
+    int x = rand() % 10;
+    int y = rand() % 10;
+
+    switch (enemy[x][y])
+    {
+        // Verfehlt
+        case 0: 
+            enemy[x][y] = 3;
+            return false;
+
+        // Treffer -> erneuter Schuss
+        case 1: 
+            enemy[x][y] = 2; 
+            return true;
+
+        // bereits getroffenes Feld -> erneuter Versuch
+        default: 
+            computerTurn(enemy);
+            break;
+    }
+}
 
 // Generiert ein zufälliges Schiff und Plaziert es in einem Array
 void generateShips(int board[10][10])
@@ -51,10 +149,13 @@ void generateShips(int board[10][10])
     {
         horizontal = rand() & 1;
 
-        if (horizontal) {
+        if (horizontal)
+        {
             x = rand() % (10 - ships[c]);
             y = rand() % (10);
-        } else {
+        } 
+        else
+        {
             x = rand() % (10);
             y = rand() % (10 - ships[c]);
         }
@@ -63,12 +164,15 @@ void generateShips(int board[10][10])
         {
             for (int i = -1; i < ships[c] + 1; i++)
             {
-                if (horizontal) {
+                if (horizontal)
+                {
                     if (board[x + i][y + j] != 0) 
                     {   
                         validShip = false;
                     }
-                } else {
+                } 
+                else
+                {
                     if (board[x + j][y + i] != 0) 
                     {   
                         validShip = false;
@@ -77,7 +181,8 @@ void generateShips(int board[10][10])
             }
         }
 
-        if (!validShip) {
+        if (!validShip)
+        {
             validShip = true;
             c--;
             continue;
@@ -99,7 +204,7 @@ void generateShips(int board[10][10])
 // "S" = Schiff
 // "X" = Treffer
 // "O" = Wasser (Kein Treffer)
-void showBoard(int p1[10][10], int com[10][10])
+void showBoards(int b1[10][10], int b2[10][10])
 {
     // Clear Screen
     // printf("\e[1;1H\e[2J");
@@ -115,7 +220,7 @@ void showBoard(int p1[10][10], int com[10][10])
         for (int col = 0; col < 10; col++)
         {
             char tile = ' ';
-            switch(p1[col][row]) {
+            switch(b1[col][row]) {
                 case 0: tile = ' '; break;
                 case 1: tile = 'S'; break;
                 case 2: tile = 'X'; break;
@@ -129,7 +234,7 @@ void showBoard(int p1[10][10], int com[10][10])
         for (int col = 0; col < 10; col++)
         {
             char tile = ' ';
-            switch(com[col][row]) {
+            switch(b2[col][row]) {
                 case 0: tile = ' '; break;
                 case 2: tile = 'X'; break;
                 case 3: tile = 'O'; break;
